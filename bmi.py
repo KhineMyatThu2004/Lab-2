4f3ef039cabc2507b330310f2fa63aa9505a37b3
KhineMyatThu2004 <amythu44@gmail.com>
2024-11-15 01:27:36 +0800

    New changes at bmi.py

---
+++ bmi.py
@@ -0,0 +1,8 @@
+def calculate_bmi(height,weight):
+    print("Height="+str(height))
+    print("Weight="+str(weight))
+    ans=weight/(height*height)
+    
+    print("The answer is "+str(ans))
+    
+calculate_bmi(weight=50,height=200)
