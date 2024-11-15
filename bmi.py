07ddb51313acd2d489156dd72d36d370268177f6
KhineMyatThu2004 <amythu44@gmail.com>
2024-11-15 20:01:22 +0800

    Make changes inside the BMI.py

--- bmi.py
+++ bmi.py
@@ -1,8 +1,18 @@
 def calculate_bmi(height,weight):
     print("Height="+str(height))
     print("Weight="+str(weight))
-    ans=weight/(height*height)
-    
-    print("The answer is "+str(ans))
-    
-calculate_bmi(weight=50,height=200)
+    bmi=(weight/(height*height))
+    print("\nBMI="+str(bmi))
+    status=""
+    if bmi<18.5:
+        status="Under Weight"
+    elif 18.5<=bmi<=25.0:
+        status="Normal Weight"
+    else:
+        status="Over Weight"
+      
+
+
+
+
+calculate_bmi (weight=57,height=1.73)
