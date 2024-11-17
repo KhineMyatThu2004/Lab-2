e7627beed5ed2cc679d9de777963e535012717fa
KhineMyatThu2004 <amythu44@gmail.com>
2024-11-17 09:58:02 +0800

    Put return type in bmi.py

--- bmi.py
+++ bmi.py
@@ -6,10 +6,13 @@ def calculate_bmi(height,weight):
     status=""
     if bmi<18.5:
         status="Under Weight"
+        return 1
     elif 18.5<=bmi<=25.0:
         status="Normal Weight"
+        return 0
     else:
         status="Over Weight"
+        return -1
       
 
 
